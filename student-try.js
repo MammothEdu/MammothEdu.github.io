9a06e520fe8fe3370200cdfe311f0796e8fd9ee1
Sebastien Brown <sebastienbrown@Sebastiens-MacBook-Pro.local>
2023-09-16 18:29:20 -0400

    Attempt API

--- student-try.js
+++ student-try.js
@@ -1,10 +1,29 @@
 const submitBtn = document.getElementById('submit');
 const output = document.getElementById('output');
+const OPENAI_API_KEY= "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";
 
 submitBtn.addEventListener('click', () => {
   const topic = document.getElementById('topic').value;
   const subject = document.getElementById('subject').value;
-  const difficulty = document.getElementById('difficulty').value;
+  const grade = document.getElementById('grade').value;
   
-  output.textContent = `${topic} - ${subject} - ${difficulty}`;
+  output.textContent = `Explain ${topic} to a ${grade} student; the subject is ${subject}`;
+  
+  const response = await fetch('https://api.openai.com/v1/completions',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${OPENAI_API_KEY}'
+    },
+    body: JSON.stringify({
+      prompt,
+      max_tokens:100,
+      temperature:0.5
+    })
+  });
+  const data = await response.json();
+
+  output.textContent=data.choices[0].text;
 });
+
+
