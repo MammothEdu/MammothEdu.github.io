224067e49c0297666460a965c10781f9dae93c87
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-16 19:10:31 -0400

    Update student.js

--- student.js
+++ student.js
@@ -1,20 +1,37 @@
-function typeOutInput() {
-    const wordsInput = document.getElementById('words').value;
-    const dropdown1Input = document.getElementById('dropdown1').value;
-    const dropdown2Input = document.getElementById('dropdown2').value;
-    const outputBox = document.getElementById('outputBox');
-    
-    const text = `Words: ${wordsInput}\nDropdown 1: ${dropdown1Input}\nDropdown 2: ${dropdown2Input}`;
-    
-    outputBox.innerHTML = ''; // Clear the output box
-    
-    let i = 0;
-    const typeInterval = setInterval(() => {
-        if (i < text.length) {
-            outputBox.innerText += text.charAt(i);
-            i++;
-        } else {
-            clearInterval(typeInterval);
-        }
-    }, 50); // Adjust the interval for typing speed
+const submitBtn = document.getElementById('submit');
+const output = document.getElementById('output');
+const OPENAI_API_KEY= "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";
+
+async function APIcall(prompt){
+  const response = await fetch('https://api.openai.com/v1/completions',{
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${OPENAI_API_KEY}`
+  },
+  body: JSON.stringify({
+    model: 'gpt-3.5-turbo',
+    messages: [{
+      role: 'user',
+      content: str
+    }]
+
+  })
+});
+const data = await response.json();
+return data.choices[0].text;
 }
+
+
+submitBtn.addEventListener('click', () => {
+  const topic = document.getElementById('topic').value;
+  const subject = document.getElementById('subject').value;
+  const grade = document.getElementById('grade').value;
+  
+  const str = `Explain ${topic} to a ${grade} student; the subject is ${subject}`;
+  
+  const aiResponse = await APIcall(str)
+
+  output.textContent=aiResponse;
+
+});
