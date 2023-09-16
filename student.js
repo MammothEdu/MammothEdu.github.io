169d951f652021d1fc4f4b9a953a538196b36597
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-16 19:43:47 -0400

    Update student.js

--- student.js
+++ student.js
@@ -1,37 +1,50 @@
-const submitBtn = document.getElementById('submit');
-const output = document.getElementById('output');
-const OPENAI_API_KEY= "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";
+// const submitBtn = document.getElementById('submit');
+// const output = document.getElementById('output');
+// const OPENAI_API_KEY= "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";
 
-async function APIcall(prompt){
-  const response = await fetch('https://api.openai.com/v1/completions',{
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${OPENAI_API_KEY}`
-  },
-  body: JSON.stringify({
-    model: 'gpt-3.5-turbo',
-    messages: [{
-      role: 'user',
-      content: str
-    }]
+// async function APIcall(prompt){
+//   const response = await fetch('https://api.openai.com/v1/completions',{
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Authorization': `Bearer ${OPENAI_API_KEY}`
+//   },
+//   body: JSON.stringify({
+//     model: 'gpt-3.5-turbo',
+//     messages: [{
+//       role: 'user',
+//       content: str
+//     }]
 
-  })
-});
-const data = await response.json();
-return data.choices[0].text;
-}
+//   })
+// });
+// const data = await response.json();
+// return data.choices[0].text;
+// }
 
 
-submitBtn.addEventListener('click', () => {
-  const topic = document.getElementById('topic').value;
-  const subject = document.getElementById('subject').value;
-  const grade = document.getElementById('grade').value;
+// submitBtn.addEventListener('click', () => {
+//   const topic = document.getElementById('topic').value;
+//   const subject = document.getElementById('subject').value;
+//   const grade = document.getElementById('grade').value;
   
-  const str = `Explain ${topic} to a ${grade} student; the subject is ${subject}`;
+//   const str = `Explain ${topic} to a ${grade} student; the subject is ${subject}`;
   
-  const aiResponse = await APIcall(str)
+//   const aiResponse = await APIcall(str)
 
-  output.textContent=aiResponse;
+//   output.textContent=aiResponse;
 
+// });
+
+const outputDiv = document.getElementById('output');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get form values
+  // display output
 });
+const subject = document.getElementById('subject').value;
+const difficulty = document.getElementById('difficulty').value;
+const question = document.getElementById('question').value;
+const output = `${subject} question on ${difficulty}: ${question}`;
+outputDiv.innerHTML = output;
