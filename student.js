2c5c135abf503243152affb1da3b409c98feca46
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-17 01:42:31 -0400

    Update student.js

--- student.js
+++ student.js
@@ -1,33 +1,33 @@
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-//   const submitBtn = document.getElementById('submit');
-//   const output = document.getElementById('output');
+  const submitBtn = document.getElementById('submit');
+  const output = document.getElementById('output');
 
-//   submitBtn.addEventListener('click', () => {
-//     let outputText = '';
-//     outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`; 
-//     outputText += textinput.value;
+  submitBtn.addEventListener('click', () => {
+    let outputText = '';
+    outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`; 
+    outputText += textinput.value;
     
-//     output.textContent += outputText;
-//   });
+    output.textContent += outputText;
+  });
 
-// });
-////////////////////////////////
-////////////////////////////////
-const form = document.getElementById('form');
-const output = document.getElementById('output');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  
-  let outputText = '';
-  
-  outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`;
-  outputText += textinput.value;
-
-  output.textContent = outputText;
 });
+////////////////////////////////
+////////////////////////////////
+// const form = document.getElementById('form');
+// const output = document.getElementById('output');
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+  
+//   let outputText = '';
+  
+//   outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`;
+//   outputText += textinput.value;
+
+//   output.textContent = outputText;
+// });
 
 
 
