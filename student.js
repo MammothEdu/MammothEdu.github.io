2e2d79befe52320828b96f659463864b30d29232
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-17 01:35:16 -0400

    Update student.js

--- student.js
+++ student.js
@@ -13,18 +13,37 @@
 //   });
 
 // });
+////////////////////////////////
+////////////////////////////////
+// const form = document.getElementById('form');
+// const output = document.getElementById('output');
 
-
-const form = document.getElementById('form');
-const output = document.getElementById('output');
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
   
-  let outputText = '';
+//   let outputText = '';
   
-  outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`;
-  outputText += textinput.value;
+//   outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`;
+//   outputText += textinput.value;
 
-  output.textContent = outputText;
+//   output.textContent = outputText;
+// });
+
+// JavaScript code 
+
+const outputDiv = document.getElementById('output');
+
+document.getElementById('submit-btn').addEventListener('click', () => {
+
+  // Get values
+  const textboxVal = document.getElementById('textbox').value;
+  const dropdown1Val = document.getElementById('dropdown1').value;
+  const dropdown2Val = document.getElementById('dropdown2').value;
+
+  // Construct output text
+  let outputText = `Text Input: ${textboxVal} \nDropdown 1: ${dropdown1Val} \nDropdown 2: ${dropdown2Val}`;
+
+  // Display output
+  outputDiv.textContent = outputText;
+
 });
