348054cc9849a5e5855c75319250c091f3dc6824
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-17 01:24:01 -0400

    Update student.js

--- student.js
+++ student.js
@@ -1,18 +1,18 @@
 
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  const submitBtn = document.getElementById('submit');
-  const output = document.getElementById('output');
+//   const submitBtn = document.getElementById('submit');
+//   const output = document.getElementById('output');
 
-  submitBtn.addEventListener('click', () => {
-    let outputText = '';
-    outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`; 
-    outputText += textinput.value;
+//   submitBtn.addEventListener('click', () => {
+//     let outputText = '';
+//     outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`; 
+//     outputText += textinput.value;
     
-    output.textContent += outputText;
-  });
+//     output.textContent += outputText;
+//   });
 
-});
+// });
 
 
 const form = document.getElementById('form');
