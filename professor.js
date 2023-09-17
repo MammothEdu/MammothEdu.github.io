ec1011a9683d14378fa53fd84b0383ec958d3015
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-17 09:21:59 -0400

    Update professor.js

--- professor.js
+++ professor.js
@@ -1,27 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   const submitBtn = document.getElementById('submit');
-  const output = document.getElementById('output');
-  const textinput = document.getElementById('Topic'); // Added textinput
+  const outputContainer = document.getElementById('output'); // Updated to target the output container
 
   submitBtn.addEventListener('click', (event) => {
     event.preventDefault(); // Prevent the default form submission behavior
 
-    let outputText = '';
-    const Subject = document.getElementById('Subject'); // Added Subject
-    const Grade = document.getElementById('Grade'); // Added Grade
+    const textinput = document.getElementById('Topic');
+    const Subject = document.getElementById('Subject');
+    const Grade = document.getElementById('Grade');
 
     if (Subject.value === 'Choose' || Grade.value === 'Choose') {
       alert('Please select a subject and grade.'); // Display an alert if subject or grade is not selected
       return;
     }
 
-    outputText += `Subject: ${Subject.value}\nGrade: ${Grade.value}\nTopic: ${textinput.value}\n\n`;
+    const outputText = `Subject: ${Subject.value}\nGrade: ${Grade.value}\nTopic: ${textinput.value}\n\n`;
 
-    // Clear previous content and append new output in a box
+    // Create a new div element for the output content
     const outputBox = document.createElement('div');
     outputBox.className = 'output-box';
     outputBox.textContent = outputText;
-    output.appendChild(outputBox);
+
+    // Append the new output to the output container
+    outputContainer.appendChild(outputBox);
 
     // Reset form fields after submission
     textinput.value = '';
@@ -29,3 +30,4 @@ document.addEventListener('DOMContentLoaded', () => {
     Grade.value = 'Choose';
   });
 });
+
