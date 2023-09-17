8d81a2803821d84d57d99dc0ecfa91f4fcbd885e
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-17 09:21:01 -0400

    Update professor.js

--- professor.js
+++ professor.js
@@ -1,12 +1,31 @@
-const submitBtn = document.getElementById('submit');
-const output = document.getElementById('output');
+document.addEventListener('DOMContentLoaded', () => {
+  const submitBtn = document.getElementById('submit');
+  const output = document.getElementById('output');
+  const textinput = document.getElementById('Topic'); // Added textinput
 
-submitBtn.addEventListener('click', () => {
-  const subject = document.getElementById('subject').value;
-  const difficulty = document.getElementById('difficulty').value;
-  const question = document.getElementById('question').value;
+  submitBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-  const outputText = `${subject} - ${difficulty}\n\n${question}`;
+    let outputText = '';
+    const Subject = document.getElementById('Subject'); // Added Subject
+    const Grade = document.getElementById('Grade'); // Added Grade
 
-  output.textContent = outputText; 
+    if (Subject.value === 'Choose' || Grade.value === 'Choose') {
+      alert('Please select a subject and grade.'); // Display an alert if subject or grade is not selected
+      return;
+    }
+
+    outputText += `Subject: ${Subject.value}\nGrade: ${Grade.value}\nTopic: ${textinput.value}\n\n`;
+
+    // Clear previous content and append new output in a box
+    const outputBox = document.createElement('div');
+    outputBox.className = 'output-box';
+    outputBox.textContent = outputText;
+    output.appendChild(outputBox);
+
+    // Reset form fields after submission
+    textinput.value = '';
+    Subject.value = 'Choose';
+    Grade.value = 'Choose';
+  });
 });
