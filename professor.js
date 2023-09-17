dfe399d7c6f230a668b84786391c1403c54cca6a
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-16 20:38:50 -0400

    Update professor.js

--- professor.js
+++ professor.js
@@ -1,1 +1,12 @@
+const submitBtn = document.getElementById('submit');
+const output = document.getElementById('output');
 
+submitBtn.addEventListener('click', () => {
+  const subject = document.getElementById('subject').value;
+  const difficulty = document.getElementById('difficulty').value;
+  const question = document.getElementById('question').value;
+
+  const outputText = `${subject} - ${difficulty}\n\n${question}`;
+
+  output.textContent = outputText; 
+});
