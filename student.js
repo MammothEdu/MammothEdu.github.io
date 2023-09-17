2ed1d258358f4f752e6830182de7b460952a6fed
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-17 01:05:38 -0400

    Update student.js

--- student.js
+++ student.js
@@ -1,12 +1,28 @@
-const submitBtn = document.getElementById('submit');
-const output = document.getElementById('output');
+// document.addEventListener('DOMContentLoaded', () => {
+// const submitBtn = document.getElementById('submit');
+// const output = document.getElementById('output');
 
-submitBtn.addEventListener('click', () => {
-  const dropdown1 = document.getElementById('dropdown1').value;
-  const dropdown2 = document.getElementById('dropdown2').value;
-  const textinput = document.getElementById('textinput').value;
+// submitBtn.addEventListener('click', () => {
+//   const dropdown1 = document.getElementById('dropdown1').value;
+//   const dropdown2 = document.getElementById('dropdown2').value;
+//   const textinput = document.getElementById('textinput').value;
 
-  const outputText = `${dropdown1} - ${dropdown2}\n\n${textinput}`;
+//   const outputText = `${dropdown1} - ${dropdown2}\n\n${textinput}`;
 
-  output.textContent = outputText;
+//   output.textContent = outputText;
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const submitBtn = document.getElementById('submit');
+  const output = document.getElementById('output');
+
+  submitBtn.addEventListener('click', () => {
+    let outputText = '';
+    outputText += `${dropdown1.value} - ${dropdown2.value}\n\n`; 
+    outputText += textinput.value;
+    
+    output.textContent += outputText;
+  });
+
 });
