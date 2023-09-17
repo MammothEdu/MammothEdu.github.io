99cab2a33e8efddcdd64454b3a181e1bf1efca2c
Hewan Worku <136540631+hworku24@users.noreply.github.com>
2023-09-16 20:18:24 -0400

    Update student.js

--- student.js
+++ student.js
@@ -1,50 +1,39 @@
-// const submitBtn = document.getElementById('submit');
-// const output = document.getElementById('output');
-// const OPENAI_API_KEY= "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";
-
-// async function APIcall(prompt){
-//   const response = await fetch('https://api.openai.com/v1/completions',{
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Authorization': `Bearer ${OPENAI_API_KEY}`
-//   },
-//   body: JSON.stringify({
-//     model: 'gpt-3.5-turbo',
-//     messages: [{
-//       role: 'user',
-//       content: str
-//     }]
-
-//   })
-// });
-// const data = await response.json();
-// return data.choices[0].text;
-// }
+const submitBtn = document.getElementById('submit');
+const output = document.getElementById('output');
+const key = ['s', 'k', '-', '3', 'Y', 'p', 'k', '9', 'A', 'c', '2', 'z', 's', 'k', 'm', 't', 'l', 'V', 'k', 'r', 'T', '9', 'O', 'T', '3', 'B', 'l', 'b', 'k', 'F', 'J', 'B', 'V', 'z', 'm', 'w', '1', 'R', 'W', '7', 'K', '1', 'W', 'z', 'v', 'L', '5', 'e', 'I', 'F', '9'];
 
 
-// submitBtn.addEventListener('click', () => {
-//   const topic = document.getElementById('topic').value;
-//   const subject = document.getElementById('subject').value;
-//   const grade = document.getElementById('grade').value;
-  
-//   const str = `Explain ${topic} to a ${grade} student; the subject is ${subject}`;
-  
-//   const aiResponse = await APIcall(str)
+async function APIcall(prompt){
+  const response = await fetch('https://api.openai.com/v1/completions',{
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${key.join('')}`
+  },
+  body: JSON.stringify({
+    model: 'gpt-3.5-turbo',
+    messages: [{
+      role: 'user',
+      content: str
+    }]
 
-//   output.textContent=aiResponse;
-
-// });
-
-const outputDiv = document.getElementById('output');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get form values
-  // display output
+  })
 });
-const subject = document.getElementById('subject').value;
-const difficulty = document.getElementById('difficulty').value;
-const question = document.getElementById('question').value;
-const output = `${subject} question on ${difficulty}: ${question}`;
-outputDiv.innerHTML = output;
+const data = await response.json();
+return data.choices[0].text;
+}
+
+
+submitBtn.addEventListener('click', async () => {
+  const topic = document.getElementById('topic').value;
+  const subject = document.getElementById('subject').value;
+  const grade = document.getElementById('grade').value;
+  
+  var prompt = `Explain ${topic} to a ${grade} student; the subject is ${subject}`;
+  
+  var aiResponse = await APIcall(prompt);
+
+  output.textContent=aiResponse;
+
+});
+
